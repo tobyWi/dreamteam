28af943692cb7755d21531fd634a78fbccad1d8b
Izabella Hertel <izabellahertel@live.se>
2017-03-15 11:46:46 +0100

    added tabs between public and private in sidebar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,15 @@
-var app = angular.module('app', ['ui.bootstrap']);
-
-
-
+var app = angular.module('app', ['ui.bootstrap', 'ui.router']);
 
 app.controller('sidebarController', ['$scope', function($scope){
 	$scope.username = 'Somebody';
+	$scope.tab = 1;
+	
+    $scope.setTab = function(newTab){
+      $scope.tab = newTab;
+    };
+
+    $scope.isSet = function(tabNum){
+      return $scope.tab === tabNum;
+    };
+
 }]);
