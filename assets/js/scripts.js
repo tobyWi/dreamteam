d7baa84adcca63d02f0f5dc3f6b622afffc81956
Izabella Hertel <izabellahertel@live.se>
2017-03-15 14:14:34 +0100

    merge conflict script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,7 @@ var app = angular.module('app', ['ui.bootstrap', 'ui.router']);
 app.controller('sidebarController', ['$scope', function($scope){
 	$scope.username = 'Somebody';
 	$scope.tab = 1;
-	
+
     $scope.setTab = function(newTab){
       $scope.tab = newTab;
     };
@@ -12,4 +12,36 @@ app.controller('sidebarController', ['$scope', function($scope){
       return $scope.tab === tabNum;
     };
 
+    $scope.x = false;
+    $scope.toggle = function() {
+        $scope.x = !$scope.x;
+    };
+
+    $scope.users = [{
+	    name: 'user',
+	    password: 'xxxxx',
+	    avatar: 'assets/img/test.jpg',
+	    online: true
+	  },
+	  {
+		name: 'user1',
+		password: 'xxxxx',
+		avatar: 'assets/img/test.jpg',
+	    online: true
+	  },
+	  {
+		name: 'user2',
+		password: 'xxxxx',
+		avatar: 'assets/img/test.jpg',
+	    online: false
+	  },
+	  {
+		name: 'user3',
+		password: 'xxxxx',
+		avatar: 'assets/img/test.jpg',
+	    online: false
+	  }
+  ];
+
 }]);
+
