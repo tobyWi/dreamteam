8ec1d33e6d5c3f99735f81df6110e21227ce9489
Izabella Hertel <izabellahertel@live.se>
2017-04-11 12:39:33 +0200

    jshint was looking at the wrong file, fixed

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp'),
 	jshint = require('gulp-jshint');
 
 gulp.task('jshint', function() {
-	return gulp.src('assets/js/*.js')
+	return gulp.src('public/assets/js/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
 });
